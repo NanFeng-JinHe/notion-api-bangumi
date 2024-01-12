0cb0c1888efd047d8f2a0a5bd93d82570f0c6232
NanFeng <3483881997@qq.com>
2024-01-12 18:34:49 +0800

    fix(util) proxy environment use script
    Addressing the issue of scripts not functioning in a proxy environment
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    </Desktop/notion-bangumi/.git/COMMIT_EDITMSG [unix] (18:34 12/01/2024)1,0-1 全部
    <s/34838/Desktop/notion-bangumi/.git/COMMIT_EDITMSG" [unix] 8L, 215B

--- util/client.py
+++ util/client.py
@@ -1,56 +1,67 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import requests
-import time
 from requests.adapters import HTTPAdapter
+import time
 
-default_header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/54.0.2840.99 Safari/537.36"
+# 默认请求头
+DEFAULT_HEADER = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/54.0.2840.99 Safari/537.36"
 }
-
 # 超时时间
-timeout = 20
-# 超时重试
-max_retries = 5
+TIMEOUT = 20
+# 超时重试次数
+MAX_RETRIES = 5
 
 
-def get(url, headers=None):
-    # 设置超时后重试https&http
+def create_session():
+    """创建会话"""
+    s = requests.Session()
+    s.trust_env = False
+    s.mount('http://', HTTPAdapter(max_retries=MAX_RETRIES))
+    s.mount('https://', HTTPAdapter(max_retries=MAX_RETRIES))
+    return s
+
+
+def retry_request(session, url, method='get', headers=None, data=None, json=None):
+    """尝试进行请求，支持重试机制"""
     if headers is None:
-        headers = default_header
-    for attempt in range(max_retries):
+        headers = DEFAULT_HEADER
+
+    for attempt in range(MAX_RETRIES):
         try:
-            s = requests.Session()
-            s.mount('http://', HTTPAdapter(max_retries))
-            s.mount('https://', HTTPAdapter(max_retries))
-            # 设置超时时间5s
-            rp = s.get(url, headers=headers, timeout=timeout)
-            rp.encoding = 'utf-8'
-            return rp
+            if method.lower() == 'get':
+                response = session.get(url, headers=headers, timeout=TIMEOUT, proxies=None)
+            elif method.lower() == 'post':
+                response = session.post(url, headers=headers, data=data, json=json, timeout=TIMEOUT, proxies=None)
+            else:
+                raise ValueError("不支持的请求类型")
+            response.raise_for_status()  # 检查请求是否成功
+            # 设置响应内容的字符编码
+            response.encoding = 'utf-8'
+            return response
         except requests.exceptions.RequestException as e:
-            print(f"第 {attempt + 1} 次连接失败:ERROR: {e}")
-            if attempt < max_retries - 1:
+            print(f"第 {attempt + 1} 次连接失败: ERROR: {e}")
+            if attempt < MAX_RETRIES - 1:
                 print("重试中....")
             else:
-                print("已达到最大重试次数,正在退出....")
+                print("已达到最大重试次数, 正在退出....")
                 break
 
     print(time.strftime('%Y-%m-%d %H:%M:%S'))
 
 
-def post(url, data=None, headers=None):
-    for attempt in range(max_retries):
-        try:
-            response = requests.post(url, data=data, headers=headers, timeout=timeout)
-            response.raise_for_status()  # 检查请求是否成功
-            print(response.text)  # 输出响应内容
-            break  # 如果请求成功，退出循环
-        except requests.exceptions.RequestException as e:
-            print(f"第 {attempt + 1} 次连接失败:ERROR: {e}")
-            if attempt < max_retries - 1:
-                print("重试中....")
-            else:
-                print("已达到最大重试次数,正在退出....")
+def get(url, headers=None):
+    """发起 GET 请求"""
+    session = create_session()
+    return retry_request(session, url, method='get', headers=headers)
 
+
+def post(url, data=None, json=None, headers=None):
+    """发起 POST 请求"""
+    session = create_session()
+    return retry_request(session, url, method='post', headers=headers, data=data, json=json)
 
 
